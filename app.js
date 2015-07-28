526dd0256b0aabdfe6348bd1fc77b132af4c5da0
Blackstock <charlieblackstock@gmail.com>
2015-07-28 16:38:38 -0600

    added JS for buttons and sort

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+// Iteration 0
 $(document).ready(function() {
-	console.log("The document is ready!");
+  console.log("The document is ready!");
   var homeValues = [1000000000, 50000, 20];
   var homeStrings = ["$1,000,000,000", "$50,000", "$20"];
   var homeImages = ["img/whiteHouse.jpg", "img/newShack.jpg", "img/shack.jpg"];
-
 
   for (var i = 0; i < 3; i++) {
     var $newHome = $('.home').first().clone().appendTo('.image-container');
@@ -12,4 +12,37 @@ $(document).ready(function() {
     $newHome.find("h4").text(homeStrings[i]);
     $newHome.find("img").attr("src", homeImages[i]);
   }
+
+  $('.dark').on('click', function() {
+    $('body').css('background', 'url(img/dark_wood.png)');
+  });
+
+  $('.light').on('click', function() {
+    $('body').css('background', 'url(img/tileable_wood_texture.png)');
+  });
+
+  $('.textures').css('width', '150px');
+  $('.textures').append("<span class='forest'></span");
+  $('.forest').css('background', 'url(img/forest.jpg)');
+
+  $('.forest').on('click', function() {
+    $('body').css({
+      'background-size' : 'cover',
+      'background' : 'url(img/forest.jpg)'
+    });
+  });
+
+// Iteration 1
+  $('.high').on('click', function() {
+    $('.image-container').html($('.image-container').children().sort(function(a, b) {
+      return b.dataset.price - a.dataset.price;
+    }));
+  });
+
+  $('.low').on('click', function() {
+    $('.image-container').html($('.image-container').children().sort(function(a, b) {
+      return a.dataset.price - b.dataset.price;
+    }));
+  });
 });
+
